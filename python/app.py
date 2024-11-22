0803cad0f606b39476c8288a180bb928115c7c91
jonas <r.jonas.p22@gmail.com>
2024-11-23 03:14:11 +0800

    update: added chat history and some minor improvement in responses

--- python/app.py
+++ python/app.py
@@ -15,26 +15,36 @@ model = model.to(device)
 # Flask app setup
 app = Flask(__name__, template_folder='../templates')
 
+# Added: keep track of the conversation history
+chat_history_ids = None
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/chat', methods=['POST'])
 def chat():
+    global chat_history_ids
     user_input = request.json.get('message', '')
     response = get_response(user_input)
     return jsonify({'response': response})
 
 def get_response(user_input):
+    global chat_history_ids
+
+    # Added: chat history
+    chat_history = f"User: {user_input} \n" 
+
     # Encode input and get response from the model
-    input_ids = tokenizer.encode(user_input + tokenizer.eos_token, return_tensors='pt').to(device)
+    input_ids = tokenizer.encode(chat_history + tokenizer.eos_token, return_tensors='pt').to(device)
 
     # Generate responses from the model
     chat_history_ids = model.generate(input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
 
     # Decode and return the response
     bot_output = tokenizer.decode(chat_history_ids[:, input_ids.shape[-1]:][0], skip_special_tokens=True)
-    return bot_output
+
+    return f"ChaTravel: {bot_output}"
 
 print("template folder path:", os.path.join(app.root_path, 'templates'))
 
